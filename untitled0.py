ad5a57809458ff19202f76153bf12513d1ce3baf
MengWalter <meng_walter@126.com>
2018-02-22 14:29:44 +0800

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 25 10:07:27 2017
+
+@author: williammeng
+"""
+import requests
+from bs4 import BeautifulSoup
+from multiprocessing import Pool
+
+url = 'https://www.zhihu.com/explore'
+
